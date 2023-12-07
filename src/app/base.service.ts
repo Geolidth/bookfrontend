7fd776266cab12b77de39eb0fc184738877322f0
Csajbók-Reményi László <myyouz74@gmail.com>
2023-12-07 19:53:10 +0100

    Init

---
+++ src/app/base.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  url='https://localhost:7204/api/Books';
+  private booksSub= new Subject();
+
+  constructor(private http: HttpClient) {
+    this.loadBooks();
+   }
+
+   loadBooks(){
+    this.http.get(this.url).subscribe(
+      (res)=>this.booksSub.next(res)
+    );
+   }
+
+   getBooks(){
+    return this.booksSub;
+   }
+
+
+   postBook(book:any){
+    this.http.post(this.url,book).subscribe(
+      {
+        next:()=>this.loadBooks(),
+        error:(err)=>console.log('Hiba a könyv felvételénél: ',err)
+      }
+    );
+   }
+
+   putBook(book:any){
+    this.http.put(this.url+'/'+book.id,book).subscribe(
+      ()=>this.loadBooks()
+    );
+   }
+
+   deleteBook(book:any){
+    this.http.delete(this.url+'/'+book.id).subscribe(
+      ()=>this.loadBooks()
+    );
+   }
+
+}
---
+++ src/app/books-list/books-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
+
+@Component({
+  selector: 'app-books-list',
+  templateUrl: './books-list.component.html',
+  styleUrls: ['./books-list.component.css']
+})
+export class BooksListComponent {
+  books:any;
+  newBook:any={};
+  oszlopok=['title','description', 'author'];
+
+  constructor(private base: BaseService){
+    this.base.getBooks().subscribe(
+      (res)=>this.books=res
+    );
+  }
+
+  postBook(){
+    this.base.postBook(this.newBook);
+    this.newBook={};
+  }
+
+  putBook(book:any){
+    this.base.putBook(book);
+  }
+
+  deleteBook(book:any){
+    this.base.deleteBook(book);
+  }
+
+}
